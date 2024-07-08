fca079ce50d18fa20d184fa53f278f5b620f2063
abraham <abrahamlawson67@gmail.com>
2024-07-08 23:15:04 +0200

    feat: Create components livewire (Settings)

---
+++ resources/views/livewire/settings.blade.php
@@ -0,0 +1,11 @@
+<div class="mg-5">
+    <div class="bg-fuchsia-900 overflow-hidden shadow-xl sm:rounded-lg p-8">
+        {{-- Titre et Boutton Créer --}}
+
+        <div class="flex justify-between">
+            <h4>Liste des Années scolaires</h4>
+            <a href="#" class="bg-blue-500 rounded-md p-4 text-black">Nouvelle Année Scolaire</a>
+        </div>
+
+    </div>
+</div>
